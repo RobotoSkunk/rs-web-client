c4afc625b523727f78b879f23d237859ec464183
RobotoSkunk <contact@robotoskunk.com>
2024-05-31 18:57:29 -0600

    Added robots.txt and sitemap.xml

---
+++ src/app/sitemap.ts
@@ -0,0 +1,18 @@
+import { MetadataRoute } from 'next';
+
+
+export default function sitemap(): MetadataRoute.Sitemap
+{
+	return [
+		{
+			url: 'https://robotoskunk.com',
+			lastModified: new Date(2024, 4, 28),
+			priority: 1,
+		},
+		{
+			url: 'https://robotoskunk.com/about',
+			lastModified: new Date(2024, 4, 29),
+			priority: 1,
+		},
+	];
+}
