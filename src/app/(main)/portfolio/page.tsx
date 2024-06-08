c8a2f512e943477eaad210cbce6763e34127450b
RobotoSkunk <contact@robotoskunk.com>
2024-06-07 20:28:14 -0600

    Added projects buttons

--- src/app/(main)/portfolio/page.tsx
+++ src/app/(main)/portfolio/page.tsx
@@ -1,16 +1,52 @@
 /* 
  * Copyright (c) 2024 Edgar Lima (RobotoSkunk) - All Rights Reserved.
  */
+'use client';
+
+import Image from 'next/image';
+import { Roboto_Condensed } from 'next/font/google';
 
 import style from './page.module.css';
 
+import exampleImage from '@/assets/img/portfolio/artworks/6.webp';
+
+
+
+const roboto = Roboto_Condensed({ weight: '300', subsets: [ 'latin' ] });
 
 export default function Page()
 {
+	const projects = [];
+
+	for (var i = 0; i < 20; i++) {
+		projects.push(
+			<button className={ style.project } onClick={ () => alert('hi') }>
+				<div className={ style.preview + ' ' + roboto.className }>
+					<Image
+						src={ exampleImage }
+						alt='example'
+						fill
+					/>
+					<div>
+						<span>Example</span>
+					</div>
+				</div>
+			</button>
+		);
+	}
+
 	return (
 		<main className={ style.main }>
 			<h1>Portfolio</h1>
-			<h2>Projects</h2>
+			<p>A few examples of the works I've done since I can remember.</p>
+
+			<section>
+				<h2>Projects</h2>
+
+				<div className={ style.gallery }>
+					{ projects }
+				</div>
+			</section>
 		</main>
 	);
 }
