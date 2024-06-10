58cddcbe93a095521c01a308b5673a91c98db267
RobotoSkunk <contact@robotoskunk.com>
2024-06-09 23:22:02 -0600

    Added PM2 example ecosystem file and installation instructions

--- README.md
+++ README.md
@@ -5,6 +5,40 @@ This is the web client of my personal website
 If you find something you think I should fix or improve, please open an issue
 and I'll review it!
 
+## Installation
+Install the client side is pretty easy, all you have to do is install all the
+dependencies first:
+
+```bash
+npm i
+```
+
+Then, run the build script.
+
+```bash
+npm run build
+```
+
+And run the project.
+
+```bash
+npm run start
+```
+
+### PM2
+If you want to use PM2, duplicate the `example.config.js`, rename it to
+`ecosystem.config.js` and configure it to your needs.
+
+```bash
+cp example.config.js ecosystem.config.js
+```
+
+Then, just simply start the PM2 daemon.
+
+```bash
+pm2 start
+```
+
 
 ## Copyright
 The source code of this project is licensed under the
---
+++ example.config.js
@@ -0,0 +1,28 @@
+/*
+ * robotoskunk.com front client. The frontend part of robotoskunk.com
+ * Copyright (C) 2024  Edgar Lima (RobotoSkunk)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
+module.exports = {
+	apps: [{
+		name               : 'rs-web-client',
+		script             : 'node_modules/next/dist/bin/next',
+		args               : `start`,
+		max_memory_restart : '150M',
+		max_restarts       : 10,
+	}],
+}
