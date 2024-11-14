b205abf753ac2c1bc663ebf07f9548ed8bee6f4d
RobotoSkunk <contact@robotoskunk.com>
2024-11-14 01:20:51 -0600

    Simplified joke redirects code

---
+++ src/data/joke-redirects.ts
@@ -0,0 +1,41 @@
+/*
+ * robotoskunk.com front client. The frontend part of robotoskunk.com
+ * Copyright (C) 2024  Edgar Lima (RobotoSkunk)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+export default [
+	'https://www.youtube.com/watch?v=hiRacdl02w4&t=46s',
+	'https://www.youtube.com/watch?v=wAMZ6KpMGQI',
+	'https://www.youtube.com/watch?v=VZzSBv6tXMw',
+	'https://www.youtube.com/watch?v=MobkO51msMI',
+	'https://www.youtube.com/watch?v=vX-PsnRXWX8',
+	'https://www.youtube.com/watch?v=5ANKM2JnZYo',
+	'https://www.youtube.com/watch?v=DmH6YPWhaDY',
+	'https://www.youtube.com/watch?v=ftYwX5hn7dY',
+	'https://www.youtube.com/watch?v=jeg_TJvkSjg',
+	'https://www.youtube.com/watch?v=AdF2uk-EscE',
+	'https://www.youtube.com/watch?v=XC_T5mvuguw',
+	'https://www.youtube.com/watch?v=WfYyBp4Ln2s',
+	'https://www.youtube.com/watch?v=Hw1ncADC9KM',
+	'https://www.youtube.com/watch?v=FR7wOGyAzpw&t=85s',
+	'https://www.youtube.com/watch?v=srZdDAJbHfc&t=10471s',
+	'https://www.youtube.com/watch?v=m7EhR8wOVxg',
+	'https://www.youtube.com/watch?v=hzVm_Cdcsew',
+	'https://www.youtube.com/watch?v=rF-O6Z3sk8c',
+	'https://www.youtube.com/watch?v=oIkhgagvrjI',
+	'https://www.youtube.com/watch?v=MLsbc-dFWS8',
+	'https://www.youtube.com/watch?v=arwACd-pdbg',
+];
--- src/middleware.ts
+++ src/middleware.ts
@@ -18,6 +18,8 @@
 
 import { NextRequest, NextResponse, userAgent } from 'next/server';
 
+import redirects from './data/joke-redirects';
+
 
 export const config = {
 	matcher: [
@@ -46,32 +48,7 @@ export function middleware(request: NextRequest)
 		case '/phpmyadmin':
 		case '/myadmin':
 		case '/admin': {
-			const redirects = [
-				'https://www.youtube.com/watch?v=hiRacdl02w4&t=46s',
-				'https://www.youtube.com/watch?v=wAMZ6KpMGQI',
-				'https://www.youtube.com/watch?v=VZzSBv6tXMw',
-				'https://www.youtube.com/watch?v=MobkO51msMI',
-				'https://www.youtube.com/watch?v=vX-PsnRXWX8',
-				'https://www.youtube.com/watch?v=5ANKM2JnZYo',
-				'https://www.youtube.com/watch?v=DmH6YPWhaDY',
-				'https://www.youtube.com/watch?v=ftYwX5hn7dY',
-				'https://www.youtube.com/watch?v=jeg_TJvkSjg',
-				'https://www.youtube.com/watch?v=AdF2uk-EscE',
-				'https://www.youtube.com/watch?v=XC_T5mvuguw',
-				'https://www.youtube.com/watch?v=WfYyBp4Ln2s',
-				'https://www.youtube.com/watch?v=Hw1ncADC9KM',
-				'https://www.youtube.com/watch?v=FR7wOGyAzpw&t=85s',
-				'https://www.youtube.com/watch?v=srZdDAJbHfc&t=10471s',
-				'https://www.youtube.com/watch?v=m7EhR8wOVxg',
-				'https://www.youtube.com/watch?v=hzVm_Cdcsew',
-				'https://www.youtube.com/watch?v=rF-O6Z3sk8c',
-				'https://www.youtube.com/watch?v=oIkhgagvrjI',
-				'https://www.youtube.com/watch?v=MLsbc-dFWS8',
-				'https://www.youtube.com/watch?v=arwACd-pdbg',
-			];
-
 			const redirect = redirects[Math.floor(Math.random() * (redirects.length - 1))];
-
 
 			return NextResponse.redirect(new URL(redirect, request.url));
 		}
