290560cefd84573dcb87ed6cbd35f2615dee1c21
RobotoSkunk <contact@robotoskunk.com>
2025-03-24 12:15:18 -0600

    Replaced all typed labels

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -54,22 +54,10 @@ export default function Root()
 			<h2>Edgar Lima</h2>
 			<p>
 				<TypeAnimation
-					preRenderFirstString={ true }
-					sequence={[
-						2000,
-						"I'm a Full Stack Developer.",
-						2000,
-						"I'm a Game Developer.",
-						2000,
-						"I'm a Linux Servers Maintainer.",
-						2000,
-						"I'm a Graphic Designer.",
-						2000,
-						"I'm a Cartoonist.",
-					]}
+					sequence={[ 'Computer Systems Engineer.' ]}
 					speed={ 50 }
-					repeat={ Infinity }
-				/>
+				>
+				</TypeAnimation>
 			</p>
 
 			<div className='social-media'>
