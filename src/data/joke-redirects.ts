ff01fca2af4dfcd785d1a87df22c84d0912ae164
RobotoSkunk <contact@robotoskunk.com>
2025-07-24 18:58:26 -0600

    The giant enemy spider

--- src/data/joke-redirects.ts
+++ src/data/joke-redirects.ts
@@ -97,4 +97,5 @@ export default [
 	'https://www.youtube.com/watch?v=xes0Yr3VTe0',
 	'https://www.youtube.com/watch?v=RxW4EhTXBaI',
 	'https://www.youtube.com/watch?v=Nj5GZIv4KjU',
+	'https://youtube.com/clip/Ugkx7sLNv0gf4N8Qez5es6tGNZAFylhRxCU0',
 ];
